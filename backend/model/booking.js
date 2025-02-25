2c435a1f4aed0c727ca165a776c1d9d422ca3571
shammahm24 <tanyaradzwam24@gmail.com>
2025-02-25 02:02:35 -0500

    Initial booking schema

---
+++ backend/model/booking.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+const Schema =  mongoose.Schema;
+
+const bookingSchema = new Schema({
+    _id : mongoose.SchemaTypes.ObjectId,
+    email : {
+        type : String,
+        required : true,
+        validator : function(v) {
+            return /^\+?[1-9]\d{1,14}$/.test(v); // E.164 international format
+        }
+    },
+    phone : {
+        type : String,
+        required : true,
+        validator : function(v) {
+            return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v); // Simple email regex
+        }
+    },
+    firstName : {
+        type : String,
+        required : true
+    },
+    lastName : {
+        type : String,
+        required : true
+    },
+    startLocation : {
+        type : locationSchema
+    },
+    endLocation : {
+        type : locationSchema
+    },
+    flightCode : {
+        type : String,
+    },
+    carType : {
+        type : String,
+    },
+    status : {
+        type : String,
+        enum : ["pending", "booked", "waiting", "driving", "complete", "cancelled"]
+    },
+    comments : {
+        types : String
+    }
+}, {timestamps : true});
+
+const locationSchema = new mongoose.Schema({
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    } });
