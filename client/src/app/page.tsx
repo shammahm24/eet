1683a5500438606f3696dde081511f89b7665156
shammahm24 <tanyaradzwam24@gmail.com>
2025-03-07 15:39:34 -0500

    Create booking form base and home page carousell

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,27 @@
 "use client"
 //import Image from "next/image";
+import BookingForm from "../components/BookingForm";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-
-      </main>
+    <div className="grid grid-rows-8 items-center justify-items-center min-h-screen p-8 pl-0 pr-0 pb-20 gap-16 sm:p-0 font-[family-name:var(--font-geist-sans)]">
+      <div className="row-span-2 row-start-1">
+        <BookingForm/>
+      </div>
+      <div className="bg-slate-500 row-span-6 row-start-3 w-full h-full">
+        <h1>page bottom</h1>
+      </div>
     </div>
   );
 }
+
+{/*
+        <main className="flex flex-col gap-8 row-start-2 justify-center sm:items-start grid-row">
+        <div className="row-span-2">
+          <BookingForm/>
+        </div>
+        <div className="bg-slate-300 row-span-3">
+          <h1>dd</h1>
+        </div>
+      </main>
+  */}
--- client/src/components/BookingForm.tsx
+++ client/src/components/BookingForm.tsx
@@ -1,1 +1,36 @@
 "use strict"
+import { FaRegArrowAltCircleRight } from 'react-icons/fa';
+
+export default function BookingForm() {
+    return(
+        <div>
+            <form className="max-w-md mx-auto">
+                <div className="grid md:grid-cols-7 md:gap-6">
+                    <div className="relative z-0 w-full mb-5 md:col-span-3 md:col-start-1 group">
+                        <input type="text" name="floating_pick_up" id="floating_pick_up" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-white peer" placeholder=" " required />
+                        <label htmlFor="floating_pick_up" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Pick up</label>
+                    </div>
+                    <div className="relative z-0 w-full mb-5 group justify-center md:col-span-1 md:col-start-4">
+                        <FaRegArrowAltCircleRight />
+                    </div>
+                    <div className="relative z-0 w-full mb-5 group md:col-span-3 md:col-start-5">
+                        <input type="text" name="floating_drop_off" id="floating_drop_off" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-white peer" placeholder=" " required />
+                        <label htmlFor="floating_drop_off" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Drop off</label>
+                    </div>
+                </div>
+                <div className="grid md:grid-cols-2 md:gap-6">
+                    <div className="relative z-0 w-full mb-5 group">
+                        <input type="text" name="floating_date" id="floating_date" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-white peer" placeholder=" " required />
+                        <label htmlFor="floating_date" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Date</label>
+                    </div>
+                    <div className="relative z-0 w-full mb-5 group">
+                        <input type="text" name="floating_time" id="floating_time" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-white peer" placeholder=" " required />
+                        <label htmlFor="floating_time" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Time</label>
+                    </div>
+                </div>
+                <button type="submit" className="text-white bg-gray-500 hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-gray-500 dark:hover:bg-gray-300 dark:focus:ring-white dark:hover:text-black">Get Quote</button>
+            </form>
+
+        </div>
+    );
+}
