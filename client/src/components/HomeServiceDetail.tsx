518aa9d8de10cdd87874d5cf90e019a1dc49f7c2
shammahm24 <tanyaradzwam24@gmail.com>
2025-05-16 17:11:28 -0400

    ...

--- client/src/components/HomeServiceDetail.tsx
+++ client/src/components/HomeServiceDetail.tsx
@@ -1,6 +1,8 @@
 "use client"
 
-export default function HomeServiceDetail({children}){
+import { ReactNode } from "react";
+
+export default function HomeServiceDetail({ children }: { children: ReactNode }) {
     return(
         <div className="grid w-full h-full items-center justify-center absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
             {children}
